3243109a2a3fbe3f2156946c62c23a92ddb55506
AlokYadav4021 <150481869+AlokYadav4021@users.noreply.github.com>
2024-02-16 17:56:21 +0530

    Add files via upload

---
+++ Music.js
@@ -0,0 +1,8 @@
+function lang(){
+    let slick = document.getElementById("click");
+    if(slick.style.display === "block"){
+        slick.style.display = "none"
+    }else{
+        slick.style.display = "block "
+    }
+}
